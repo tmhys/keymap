9db4300b09d6fd669ddc876e0969ca0575682e94
tmhys <t.h.tomy@gmail.com>
2021-03-28 23:08:17 +0900

    update

--- keymap.c
+++ keymap.c
@@ -53,11 +53,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_QWERTY] = LAYOUT(\
-		  KC_ESC,     KC_1,    KC_2,    KC_3,    KC_4,  KC_5,   KC_6,    						KC_7,    KC_8,   KC_9,  	 KC_0,    KC_MINS, KC_EQL,  KC_GRV, \
-		  KC_TAB,     KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,   KC_RBRC, 						KC_LBRC, KC_Y,   KC_U,  	 KC_I,    KC_O,    KC_P,    KC_DEL,  \
-		  M_HNZN_TGL, KC_A,    KC_S,    KC_D,    KC_F,  KC_G,   KC_ENT,  						KC_BSPC, KC_H,   KC_J,  	 KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-		  KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,   KC_LSFT ,  						KC_RALT, KC_N,   KC_M,  	 KC_COMM, KC_DOT,  KC_UP,   KC_NUBS, \
-		  KC_LCTL,    KC_LGUI, KC_LALT, KC_LCTL, 		LOWER,  KC_ESC,	KC_SPC,			KC_SPC, KC_ENT,  RAISE,  KC_SLSH, 	 		  KC_LEFT, KC_DOWN, KC_RGHT	 \
+		  KC_ESC,     KC_1,    KC_2,    KC_3,    KC_4,  KC_5,   KC_6,    						KC_7,    KC_8,   KC_9,  	 KC_0,    KC_MINS, KC_EQL,  KC_BSPC, \
+		  KC_TAB,     KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,   KC_LBRC, 						KC_RBRC, KC_Y,   KC_U,  	 KC_I,    KC_O,    KC_P,    KC_GRV,  \
+		  M_HNZN_TGL, KC_A,    KC_S,    KC_D,    KC_F,  KC_G,   KC_DEL,  						KC_BSPC, KC_H,   KC_J,  	 KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+		  KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,   KC_TAB ,  						KC_SPC, KC_N,   KC_M,  	 KC_COMM, KC_DOT,  KC_UP,   KC_NUBS, \
+		  KC_LCTL,    KC_LGUI, KC_LALT, KC_LCTL, 		LOWER,  KC_ESC,	KC_SPC,		   KC_RSFT, KC_ENT,  RAISE,  KC_SLSH, 	 		  KC_LEFT, KC_DOWN, KC_RGHT	 \
 		  ),
 
   /* Lower
@@ -77,11 +77,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_LOWER] = LAYOUT(\
 		  KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,  						 KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR, \
 		  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,						 XXXXXXX, XXXXXXX, XXXXXXX, KC_INS,  XXXXXXX, XXXXXXX, XXXXXXX, \
-		  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,						 KC_BTN4, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, XXXXXXX, XXXXXXX, \
-		  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,						 XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
-		  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, 		   XXXXXXX, XXXXXXX, XXXXXXX,	    XXXXXXX, XXXXXXX, XXXXXXX,			XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX	\
+		  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,						 XXXXXXX, KC_LEFT, KC_DOWN, KC_UP, 	KC_RGHT, XXXXXXX, XXXXXXX, \
+		  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,						 XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PGUP, XXXXXXX, \
+		  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, 		   XXXXXXX, XXXXXXX, XXXXXXX,	    XXXXXXX, XXXXXXX, XXXXXXX,			XXXXXXX, KC_HOME, KC_PGDN, KC_END	\
 		  ),
-
    /* Raise
    * ,----------------------------------------------------------------------------------------------------------------------.
    * |  F11 |  F1  |  F2  |  F3  |  F4  |  F5  |   {  |                    |   }  |  F6  |  F7  |  F8  |  F9  |  F10 |  F12 |
@@ -99,8 +98,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		  KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   							KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR, \
 		  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, 							XXXXXXX, XXXXXXX, XXXXXXX, KC_INS,  XXXXXXX, XXXXXXX, XXXXXXX, \
 		  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, 							XXXXXXX, KC_LEFT, KC_DOWN, KC_UP, 	KC_RGHT, XXXXXXX, XXXXXXX, \
-		  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, 							XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
-		  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, 		   XXXXXXX, XXXXXXX, XXXXXXX, 		   XXXXXXX, XXXXXXX, XXXXXXX, 		   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX  \
+		  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,						 XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PGUP, XXXXXXX, \
+		  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, 		   XXXXXXX, XXXXXXX, XXXXXXX,	    XXXXXXX, XXXXXXX, XXXXXXX,			XXXXXXX, KC_HOME, KC_PGDN, KC_END	\
 		  ),
 
   /* Adjust
